b110975a111739fbfa27a2c37362ad070a6f3583
Nikita Kad <nikita0905@users.noreply.github.com>
2020-03-02 18:12:56 -0600

    Update urls.py

--- api/api_server/urls.py
+++ api/api_server/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/execute/', views.Execute.as_view()),
+    path('execute/', views.Execute.as_view()),
     path('specs/', views.Specs.as_view()),
     path('authentication-token/', views.AuthenticationToken.as_view()),
     path('save/', views.Save.as_view()),
